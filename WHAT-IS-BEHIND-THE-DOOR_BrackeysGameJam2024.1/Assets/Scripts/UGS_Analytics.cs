8322394ad504eb282f70bd0c59219f2bf7864250
flyupunity <flyupworking@gmail.com>
2024-02-25 13:05:07 +0700

    Add Unity Analytics, add warnings
    edit .gitignore
    13:03 25.02.2024

--- WHAT-IS-BEHIND-THE-DOOR_BrackeysGameJam2024.1/Assets/Scripts/UGS_Analytics.cs
+++ WHAT-IS-BEHIND-THE-DOOR_BrackeysGameJam2024.1/Assets/Scripts/UGS_Analytics.cs
@@ -1,14 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using Unity.Services.Analytics;
-//using Unity.Services.Core;
-//using Unity.Services.Core.Analytics;
-//using UnityEngine.SceneManagement;
-//using UnityEngine.Analytics;
+using Unity.Services.Analytics;
+using Unity.Services.Core;
+using Unity.Services.Core.Analytics;
+using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
 
 public class UGS_Analytics : MonoBehaviour
 {
+        async void Start()
+    {
+        try
+        {
+            await UnityServices.InitializeAsync();
+            GiveConsent(); //Get user consent according to various legislations
+        }
+        catch (ConsentCheckException e)
+        {
+            Debug.Log(e.ToString());
+        }
+    }
+
+    public void GiveConsent()
+    {
+        // Call if consent has been given by the user
+        AnalyticsService.Instance.StartDataCollection();
+        Debug.Log($"Consent has been provided. The SDK is now collecting data!");
+    }
     /*public void StartingMemCustomEvent()
     {
         try
@@ -20,7 +39,7 @@ public class UGS_Analytics : MonoBehaviour
         {
             Debug.Log(e.ToString());
         }
-    }*/
+    }
     public void MemCustomEvent()
     {
         //int currentLevel = Random.Range(1, 4);
@@ -34,5 +53,5 @@ public class UGS_Analytics : MonoBehaviour
         AnalyticsService.Instance.Flush();
         */
         //Analytics.FlushEvents();
-   }
+   //}
 }
